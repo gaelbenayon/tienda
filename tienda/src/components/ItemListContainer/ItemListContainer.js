111515948a81b425ab046ea1af87d0b09281cb32
gaelbenayon <gaelbenayon@gmail.com>
2023-07-17 22:26:10 -0300

    useState

--- tienda/src/components/ItemListContainer/ItemListContainer.js
+++ tienda/src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,24 @@
+import {useState} from "react";
+
 function ItemListContainer({greeting}) {
+    const [cantidadDiscos, setCantidadDiscos] = useState(0);
+
+    function handleClickSumarDisco() {
+        setCantidadDiscos(cantidadDiscos + 1);
+    }
+
+    function handleClickRestarDisco() {
+        setCantidadDiscos(cantidadDiscos - 1);
+    }
+
     return (
-        <h1 className="text-center p-1">{greeting}</h1>
-    )
+        <div>
+            <h1 className="text-center p-1">{greeting}</h1>
+            <p>Discos: {cantidadDiscos}</p>
+            <button onClick={handleClickSumarDisco}>Sumar disco</button>
+            <button onClick={handleClickRestarDisco}>Restar disco</button>
+        </div>
+    )   
 }
 
 export default ItemListContainer;
